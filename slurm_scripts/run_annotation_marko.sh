f9cda1e687d7367cd2b5bb3bd65aed6132748089
MarkoHaralovic <mharalovic11@gmail.com>
2025-12-12 16:37:29 +0100

    Loaded SenWave and COVID-19-NLP data

--- slurm_scripts/run_annotation_marko.sh
+++ slurm_scripts/run_annotation_marko.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=qwen_annotation
 #SBATCH --partition=students
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=32
 #SBATCH --time=01:00:00
 #SBATCH --mem=40G
-#SBATCH --output=/home/s3758869/synchain-absa-emotion/outputs/qwen_annotation_%j.out
-#SBATCH --error=/home/s3758869/synchain-absa-emotion/outputs/qwen_annotation_%j.err
+#SBATCH --output=/home/s3758869/synchain-absa-emotion/slurm_outputs/qwen_annotation_%j.out
+#SBATCH --error=/home/s3758869/synchain-absa-emotion/slurm_outputs/qwen_annotation_%j.err
 
 echo "Job ID: $SLURM_JOB_ID"
 echo "GPU Information:"
