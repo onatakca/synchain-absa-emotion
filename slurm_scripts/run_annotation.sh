4180aa969d68ba137cafc055f0f46f1c6640a0a9
MarkoHaralovic <mharalovic11@gmail.com>
2025-12-12 16:03:54 +0100

    Organized slurm scripts

--- slurm_scripts/run_annotation.sh
+++ slurm_scripts/run_annotation.sh
@@ -7,13 +7,12 @@
 #SBATCH --output=annotation_conversational_%j.log
 
 echo "Job ID: $SLURM_JOB_ID"
-
-cd /home/s3758869/synchain-absa-emotion 
-
-export PYTHONPATH="/home/s3758869/synchain-absa-emotion:$PYTHONPATH"
-
 echo "GPU Information:"
 nvidia-smi --query-gpu=name,memory.total,memory.free --format=csv
 echo ""
 
+
+export PYTHONPATH="/home/s3758869/synchain-absa-emotion:$PYTHONPATH"
+
+cd /home/s3758869/synchain-absa-emotion 
 python3 scripts/annotation/annotate.py
---
+++ slurm_scripts/run_annotation_marko.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=qwen_annotation
+#SBATCH --partition=students
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=32
+#SBATCH --time=01:00:00
+#SBATCH --mem=40G
+#SBATCH --output=/home/s3758869/synchain-absa-emotion/outputs/qwen_annotation_%j.out
+#SBATCH --error=/home/s3758869/synchain-absa-emotion/outputs/qwen_annotation_%j.err
+
+echo "Job ID: $SLURM_JOB_ID"
+echo "GPU Information:"
+nvidia-smi --query-gpu=name,memory.total,memory.free --format=csv
+echo ""
+
+source /home/s3758869/absa_synchain/bin/activate
+cd /home/s3758869/synchain-absa-emotion
+python scripts/annotation/annotate.py
