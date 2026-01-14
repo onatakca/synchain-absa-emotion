58e68ed72b428f92e67a77b417ada2489d166a49
MarkoHaralovic <mharalovic11@gmail.com>
2026-01-14 18:49:53 +0100

    Added lora for finetuning

--- scripts/modeling/knowledge_distillation.py
+++ scripts/modeling/knowledge_distillation.py
@@ -1,10 +1,12 @@
-from transformers import Trainer, TrainingArguments, AutoModelForCausalLM, AutoTokenizer
+from transformers import Trainer, TrainingArguments, AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from scripts.modeling.dataset import ABSADataset, get_data, Collator
 from scripts.modeling.evaluate import EvaluationCallback
 import json
 from datetime import datetime
 from pathlib import Path
 import os
+import torch
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 
 TRAIN_FILES = [
     "/home/s3758869/synchain-absa-emotion/data/output_data/Qwen25-32b-instruct_annotation/covidsenti_chunk1_annotated.json",
@@ -18,11 +20,11 @@ TRAIN_FILES = [
     # "/home/s3758869/synchain-absa-emotion/data/output_data/Qwen25-32b-instruct_annotation/covidsenti_chunk9_annotated.json",
     # "/home/s3758869/synchain-absa-emotion/data/output_data/Qwen25-32b-instruct_annotation/covidsenti_chunk10_annotated.json",
     # "/home/s3758869/synchain-absa-emotion/data/output_data/Qwen25-32b-instruct_annotation/covidsenti_chunk11_annotated.json",
+    # "/home/s3758869/synchain-absa-emotion/data/output_data/Qwen25-32b-instruct_annotation/covid19nlp_chunk0_annotated.json"
 ]
 
 VALIDATION_FILES = [
     "/home/s3758869/synchain-absa-emotion/data/output_data/Qwen25-32b-instruct_annotation/covidsenti_chunk0_annotated.json",
-    # "/home/s3758869/synchain-absa-emotion/data/output_data/Qwen25-32b-instruct_annotation/covid19nlp_chunk0_annotated.json"
 ]
 
 config_file_path = "/home/s3758869/synchain-absa-emotion/scripts/modeling/configs/full_pipeline.json"
@@ -30,20 +32,9 @@ config_file_path = "/home/s3758869/synchain-absa-emotion/scripts/modeling/config
 with open(config_file_path, "r") as f:
     config = json.load(f)
     
-train_dataset = ABSADataset(get_data(TRAIN_FILES), tasks = config["tasks"])
-val_dataset = ABSADataset(get_data(VALIDATION_FILES), tasks=  config["tasks"])
+train_dataset = ABSADataset(get_data(TRAIN_FILES), tasks = config["tasks"], max_samples = config["max_samples"])
+val_dataset = ABSADataset(get_data(VALIDATION_FILES), tasks=  config["tasks"], max_samples= config["max_samples"])
 
-student_tokenizer = AutoTokenizer.from_pretrained(
-    config["model_identifier"],
-    local_files_only=True
-    )
-
-student_model = AutoModelForCausalLM.from_pretrained(
-    config["model_identifier"],
-    local_files_only=True
-    )
-
-collator = Collator(tokenizer=student_tokenizer, max_length=config["max_length"])
 
 run_id = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 model_name = config["model_identifier"].split("/")[-1]
@@ -60,6 +51,43 @@ for p in [ckpt_dir, logs_dir, eval_dir]:
 with open(run_dir / "config.json", "w") as f:
     json.dump(config, f, indent=2, sort_keys=True)
     
+student_tokenizer = AutoTokenizer.from_pretrained(
+    config["model_identifier"],
+    local_files_only=True,
+    padding_side='left'
+    )
+collator = Collator(tokenizer=student_tokenizer, max_length=config["max_length"])
+
+bnb_config = BitsAndBytesConfig(
+    load_in_8bit=True,
+    load_in_4bit=False,    
+)
+
+student_model = AutoModelForCausalLM.from_pretrained(
+    config["model_identifier"],
+    local_files_only=True,
+    torch_dtype=torch.bfloat16 if config.get("bf16", False) else torch.float32,
+    quantization_config=bnb_config,
+    device_map="auto",  
+)
+
+student_model.config.use_cache = False 
+student_model = prepare_model_for_kbit_training(student_model)
+
+if config.get("gradient_checkpointing", False):
+    student_model.gradient_checkpointing_enable()
+
+lora_config = LoraConfig(
+    r=config["lora_r"],
+    lora_alpha=config["lora_alpha"],
+    lora_dropout=config["lora_dropout"],
+    bias="none",
+    task_type="CAUSAL_LM",
+    target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
+)
+
+student_model = get_peft_model(student_model, lora_config)
+student_model.print_trainable_parameters()
 
 training_args = TrainingArguments(
     output_dir=ckpt_dir,
@@ -76,7 +104,10 @@ training_args = TrainingArguments(
     greater_is_better=False,
     gradient_accumulation_steps=config["gradient_accumulation_steps"],
     dataloader_num_workers=min(os.cpu_count(),config["dataloader_num_workers"]),
-    eval_strategy="epoch",
+    eval_strategy="no",
+    bf16=config.get("bf16", False),
+    optim="paged_adamw_8bit", 
+    gradient_checkpointing=config.get("gradient_checkpointing", False),
     save_strategy="steps",
     save_steps=config["save_steps"],
     save_total_limit=config["save_total_limit"],
